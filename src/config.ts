06a71329f3f99a082b88ad6dc6cc2987a2cdb877
Brice Leroy <bbrriiccee@gmail.com>
2020-04-16 08:01:25 -0700

    better logging on startup

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
+import { log, bold } from "./log";
+
 export const config = {
   port: 8080,
   datafile: "database.json",
@@ -5,13 +7,16 @@ export const config = {
 };
 
 export function setDatafile(datafile: string) {
+  log(`Database file is ${bold(datafile)}`);
   config.datafile = datafile;
 }
 
 export function setPort(port: number) {
+  log(`Using port ${bold(port)}`);
   config.port = port;
 }
 
 export function setDelay(delay: number) {
+  log(`Set delay to ${bold(delay)}`);
   config.delay = delay;
 }
--- src/log.ts
+++ src/log.ts
@@ -1,3 +1,7 @@
 export function log(...args: any[]) {
   console.log("\x1b[36m%s\x1b[0m", `[${new Date().toLocaleString()}]`, ...args);
 }
+
+export function bold(value: string | number | boolean): string {
+  return `\x1b[1m${value}\x1b[0m`;
+}
