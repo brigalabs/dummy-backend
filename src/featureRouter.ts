ff3e7e103741021dcc38c28d8339f9125269911f
Brice Leroy <bbrriiccee@gmail.com>
2020-04-16 17:51:50 -0700

    Handle CORS for features

--- src/featureRouter.ts
+++ src/featureRouter.ts
@@ -4,7 +4,9 @@ import { upload } from "./features/upload";
 import { getFile } from "./features/getFile";
 import { startsWith } from "lodash";
 import { notFoundResponse } from "./response";
+import { handleOption } from "./handlers";
 
+const isOptions = matches({ method: "OPTIONS" });
 const isUpload = matches({ url: "/_upload", method: "PUT" });
 const isGetFile = matches({ method: "GET" });
 
@@ -16,6 +18,10 @@ export function featureRouter(
     url: req.url,
     method: req.method,
   };
+
+  if (isOptions(request)) {
+    return handleOption(req, res);
+  }
 
   if (isUpload(request)) {
     return upload(req, res);
