130fdd9d6114c932fbbab0e79cd7d00e0b5886bf
Brice Leroy <brice@brigalabs.com>
2021-03-29 07:18:07 -0700

    Add error logger

--- src/log.ts
+++ src/log.ts
@@ -5,3 +5,7 @@ export function log(...args: any[]) {
 export function bold(value: string | number | boolean): string {
   return `\x1b[1m${value}\x1b[0m`;
 }
+
+export function errorLog(...args: any[]) {
+  console.log("\x1b[31m%s\x1b[0m", `[${new Date().toLocaleString()}]`, ...args);
+}
